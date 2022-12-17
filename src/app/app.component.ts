782ab3f79ca82ac3e9e3f4f6806689431a82ec18
Mandeeptq79 <120645256+Mandeeptq79@users.noreply.github.com>
2022-12-17 20:26:01 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component, VERSION } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ViewChild,
+  AfterViewInit,
+  NgZone,
+} from '@angular/core';
+import { TableVirtualScrollDataSource } from 'ng-table-virtual-scroll';
+import { filter, map, pairwise, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  @ViewChild(CdkVirtualScrollViewport) scroller: CdkVirtualScrollViewport;
+  listItems = [];
+  constructor(private ngZone: NgZone) {}
+  displayedColumns = ['id', 'name'];
+  ngOnInit() {
+    this.fetchMore();
+  }
+  ngAfterViewInit() {
+    this.scroller
+      .elementScrolled()
+      .pipe(
+        map(() => this.scroller.measureScrollOffset('bottom')),
+        pairwise(),
+        filter(([y1, y2]) => y2 < y1 && y2 < 140),
+        throttleTime(200)
+      )
+      .subscribe(() => {
+        this.ngZone.run(() => {
+          this.fetchMore();
+        });
+      });
+  }
+  fetchMore(): void {
+    const newItems = [];
+    for (let i = 1; i < 20; i++) {
+      newItems.push({
+        id: i,
+        name: `Element #${i}`,
+      });
+    }
+    /* const DATA = Array.from({ length: 20 }, (v, i) => ({
+      id: i + 1,
+      name: `Element #${i + 1}`,
+    })); */
+    this.listItems = [...this.listItems, ...newItems];
+  }
+  //dataSource = new TableVirtualScrollDataSource(this.listItems);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatTableModule } from '@angular/material/table';
+import { TableVirtualScrollModule } from 'ng-table-virtual-scroll';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ScrollingModule,
+    MatTableModule,
+    TableVirtualScrollModule,
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
