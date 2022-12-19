38f07cdaa85e6828dfb8e1dd8667e1c36bad8f0c
Mandeeptq79 <120645256+Mandeeptq79@users.noreply.github.com>
2022-12-19 18:05:16 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import {
+  VIRTUAL_SCROLL_STRATEGY,
+  CdkVirtualScrollViewport,
+} from '@angular/cdk/scrolling';
 import {
   Component,
   OnInit,
@@ -6,23 +9,71 @@ import {
   ViewChild,
   AfterViewInit,
   NgZone,
+  Inject,
 } from '@angular/core';
 import { TableVirtualScrollDataSource } from 'ng-table-virtual-scroll';
-import { filter, map, pairwise, throttleTime } from 'rxjs';
-
+import {
+  Observable,
+  of,
+  combineLatest,
+  filter,
+  map,
+  pairwise,
+  throttleTime,
+} from 'rxjs';
+import { TableVirtualScrollStrategy } from './virtualscrollstrategy.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   changeDetection: ChangeDetectionStrategy.Default,
+  providers: [
+    {
+      provide: VIRTUAL_SCROLL_STRATEGY,
+      useClass: TableVirtualScrollStrategy,
+    },
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // Manually set the amount of buffer and the height of the table elements
+  static BUFFER_SIZE = 3;
+  rowHeight = 48;
+  headerHeight = 56;
   @ViewChild(CdkVirtualScrollViewport) scroller: CdkVirtualScrollViewport;
+  dataSource: Observable<Array<any>>;
+  gridHeight = 400;
   listItems = [];
-  constructor(private ngZone: NgZone) {}
+  rows: Observable<Array<any>>;
+
   displayedColumns = ['id', 'name'];
+  constructor(
+    private ngZone: NgZone,
+    @Inject(VIRTUAL_SCROLL_STRATEGY)
+    private readonly scrollStrategy: TableVirtualScrollStrategy
+  ) {}
+
   ngOnInit() {
+    this.rows = of(this.listItems);
+    const range =
+      Math.ceil(this.gridHeight / this.rowHeight) + AppComponent.BUFFER_SIZE;
+    this.scrollStrategy.setScrollHeight(this.rowHeight, this.headerHeight);
     this.fetchMore();
+    console.log(this.listItems);
+    console.log(this.rows);
+    this.dataSource = combineLatest([
+      this.rows,
+      this.scrollStrategy.scrolledIndexChange,
+    ]).pipe(
+      map((value: any) => {
+        // Determine the start and end rendered range
+        const start = Math.max(0, value[1] - AppComponent.BUFFER_SIZE);
+        const end = Math.min(value[0].length, value[1] + range);
+
+        // Update the datasource for the rendered range of data
+        return value[0].slice(start, end);
+      })
+    );
+    console.log(this.dataSource);
   }
   ngAfterViewInit() {
     this.scroller
